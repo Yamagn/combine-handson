6759fd8d35eb389d037e9608b2400648f8442792
Yamagn <syankenpon@gmail.com>
2021-06-25 13:39:51 +0900

    Initial Commit

---
+++ combine-handson/combine_handsonApp.swift
@@ -0,0 +1,17 @@
+//
+//  combine_handsonApp.swift
+//  combine-handson
+//
+//  Created by ymgn on 2021/06/25.
+//
+
+import SwiftUI
+
+@main
+struct combine_handsonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
